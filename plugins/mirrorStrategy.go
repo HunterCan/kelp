HunterCan/kelp

commit 8cf3e679ec9bc11f2d929e6f79105dcb3ae8de87

mirror strategy fix volume and amount for modify offers, closes #526
during modify offers when:
a. length of new offers >= length of old offers
b. length of new offers < length of old offers

create new offers when length of new offers >= length of old offers was doing the correct thing

plugins/mirrorStrategy.go
@@ -468,6 +468,10 @@ func (s *mirrorStrategy) updateLevels(
 	deleteOps := []txnbuild.Operation{}
 	if len(newOrders) >= len(oldOffers) {
 		for i := 0; i < len(oldOffers); i++ {
+			// TODO NS - don't modify existing variables
+			newOrders[i].Price = newOrders[i].Price.Scale(priceMultiplier)
+			newOrders[i].Volume = newOrders[i].Volume.Scale(1.0 / s.volumeDivideBy)
+
 			modifyOp, deleteOp, e := s.doModifyOffer(oldOffers[i], newOrders[i], priceMultiplier, modifyOffer, hackPriceInvertForBuyOrderChangeCheck)
 			if e != nil {
 				return nil, nil, e
@@ -514,6 +518,10 @@ func (s *mirrorStrategy) updateLevels(
 		}
 	} else {
 		for i := 0; i < len(newOrders); i++ {
+			// TODO NS - don't modify existing variables
+			newOrders[i].Price = newOrders[i].Price.Scale(priceMultiplier)
+			newOrders[i].Volume = newOrders[i].Volume.Scale(1.0 / s.volumeDivideBy)
+
 			modifyOp, deleteOp, e := s.doModifyOffer(oldOffers[i], newOrders[i], priceMultiplier, modifyOffer, hackPriceInvertForBuyOrderChangeCheck)
 			if e != nil {
 				return nil, nil, e

commit 0a2b14d36863eac00fd6bf66358f3406af034873

fix bug introduced in 8cf3e679ec9bc11f2d929e6f79105dcb3ae8de87, closes #526

plugins/mirrorStrategy.go
@@ -556,8 +556,8 @@ func (s *mirrorStrategy) doModifyOffer(
 	modifyOffer func(offer hProtocol.Offer, price float64, amount float64, incrementalNativeAmountRaw float64) (*txnbuild.ManageSellOffer, error),
 	hackPriceInvertForBuyOrderChangeCheck bool, // needed because createBuy and modBuy inverts price so we need this for price comparison in doModifyOffer
 ) (txnbuild.Operation, txnbuild.Operation, error) {
-	price := newOrder.Price.Scale(priceMultiplier)
-	vol := newOrder.Volume.Scale(1.0 / s.volumeDivideBy)
+	price := newOrder.Price
+	vol := newOrder.Volume
 	oldPrice := model.MustNumberFromString(oldOffer.Price, s.primaryConstraints.PricePrecision)
 	oldVol := model.MustNumberFromString(oldOffer.Amount, s.primaryConstraints.VolumePrecision)
 	if hackPriceInvertForBuyOrderChangeCheck {